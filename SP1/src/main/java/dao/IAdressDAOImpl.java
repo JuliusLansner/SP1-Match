f627479b92765092330853d59b8da5b754876d32
Julius Lansner <axiiss@live.dk>
2023-09-05 11:49:02 +0200

    US-6 method Implemented

--- SP1/src/main/java/dao/IAdressDAOImpl.java
+++ SP1/src/main/java/dao/IAdressDAOImpl.java
@@ -1,7 +1,38 @@
 package dao;
 
-public class IAdressDAOImpl {
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import model.Adress;
+import model.Person;
+
+import java.util.List;
+
+public class IAdressDAOImpl implements IAdressDAO {
     //CRUD if it makes sense
     //Follow singleton pattern
+    private static EntityManagerFactory emf;
+    private static IAdressDAOImpl adressDAO;
 
+
+    private IAdressDAOImpl(EntityManagerFactory emf){
+        this.emf = emf;
+    }
+    public static IAdressDAOImpl getInstance(EntityManagerFactory emf){
+        if(adressDAO == null){
+            adressDAO = new IAdressDAOImpl(emf);
+        }
+        return adressDAO;
+
+    }
+    @Override
+    public List<Person> cityPersonLivingList(String cityName) {
+        try(EntityManager em = emf.createEntityManager()){
+            TypedQuery<Person> query = em.createQuery("SELECT p FROM Person p JOIN p.adress a WHERE a.zipCode.cityName = :cityName",Person.class);
+            query.setParameter("cityName",cityName);
+            return query.getResultList();
+        }
+
+    }
 }
